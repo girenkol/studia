dec6fc01bee8609c77575a683b10be1f8d876e02
girenkol <82716636+girenkol@users.noreply.github.com>
2023-10-24 22:11:00 +0200

    Update oct24_MetrykaKsiazki.c

--- programy/oct24_MetrykaKsiazki.c
+++ programy/oct24_MetrykaKsiazki.c
@@ -14,7 +14,32 @@ typedef struct element
     struct element* nastepny;
 } element;
 
+element* utworz_element(element* glowa, metryka_ksiazki ksiazka)
+{
+    if(glowa)
+    {
+        if (glowa -> nastepny)
+            return utworz_element(glowa->nastepny, ksiazka);
+        else
+        {
+            glowa->nastepny = malloc(sizeof(element));
+            *(glowa->nastepny) = (element) { .ksiazka = ksiazka, .nastepny = NULL};
+            return glowa;
+        }
+    }
+    else
+    {
+        element* ptr = malloc(sizeof(element));
+        *ptr = (element) { .ksiazka = ksiazka, .nastepny = NULL };
+        return ptr;
+    }
+}
+
 void main()
 {
+    element* glowa = NULL;
 
+    glowa = utworz_element(glowa, (metryka_ksiazki) { .tytul = "Przygody Kubusia Puchatka", .autor = "A. A. Milne", .rok_wydania = 1929, .liczba_stron = 124, .cena = 36.7});
+
+    glowa = utworz_element(glowa, (metryka_ksiazki) { .tytul = "Dziwne przypadki Prosiaczka", .autor = "A. A. Milne", .rok_wydania = 1932, .liczba_stron = 87, .cena = 66.32});
 }
