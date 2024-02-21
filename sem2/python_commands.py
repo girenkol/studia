983149778deeacb7a3d9241f7950a764a168c800
girenkol <82716636+girenkol@users.noreply.github.com>
2024-02-21 15:20:26 +0100

    Update and rename python_commands to python_commands.py

---
+++ sem2/python_commands.py
@@ -0,0 +1,9 @@
+L = [i*j*k for i in range(10) for j in range(10) for k in range(10)]
+[i*i+1 for i in range(0,10,2)]
+L[3:2:-1]
+//Liste można trzymać w liście, można trzymać różne typy zmiennych
+
+//dla takiego czegoś
+K = (0,1) - krotka
+nie można zrobić K[0]=9
+
