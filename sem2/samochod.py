15a1d2c716dc0239c238fc2dd3b09fd5bb57a08b
girenkol <82716636+girenkol@users.noreply.github.com>
2024-03-13 14:49:22 +0100

    Create samochod.py

---
+++ sem2/samochod.py
@@ -0,0 +1,18 @@
+class Samochod:
+
+    NumerySeryjne = 1
+
+    def __init__(self, NumerRejestracyjny):
+        self.MojNumerRejestracyjny = NumerRejestracyjny
+        self.MojNumerSeryjny = Samochod.NumerySeryjne
+        Samochod.NumerySeryjne += 1
+
+    def UruchomSilnik(self):
+        print("Silnik samochodu o numrze seryjnym {0} i numerze rejestracyjnym {1} zostal uruchomiony".format(self.MojNumerSeryjny,self.MojNumerRejestracyjny))
+
+if __name__ == "__main__":
+
+    s1 = Samochod("XD 666")
+    s1.UruchomSilnik()
+    s2 = Samochod("JP 2137N")
+    s2.UruchomSilnik()
