953f226cd3ecd5f6a518c84e61fff22beaa51b59
girenkol <82716636+girenkol@users.noreply.github.com>
2024-04-20 16:47:52 +0200

    Update DIJKSTRA.py

--- sem2/DIJKSTRA.py
+++ sem2/DIJKSTRA.py
@@ -1,43 +1,60 @@
-class Graf():
+import math
 
+class Graf():
     def __init__(self, wierzcholki, krawedzie):
         if len(wierzcholki) != len(set(wierzcholki)):
-            raise Exception("Nazwy wierzcholkow powtarzaja sie")
+            raise Exception("Nazwy wierzchołków powtarzają się")
         for krawedz in krawedzie:
             if not krawedz[0][0] in wierzcholki or not krawedz[0][1] in wierzcholki:
                 raise Exception("Nieprawidłowe krawędzie")
 
         self.wierzcholki = wierzcholki
         self.krawedzie = krawedzie
+        self.graf = self.stworz_graf()
+
+    def stworz_graf(self):
+        graf = {}
+        for w in self.wierzcholki:
+            graf[w] = {}
+        for (w1, w2), waga in self.krawedzie:
+            graf[w1][w2] = waga
+        return graf
+    
+    def znajdz_sciezke(self, p, wierzcholek):
+        sciezka = []
+        while wierzcholek is not None:
+            sciezka.insert(0, wierzcholek)
+            wierzcholek = p[wierzcholek]
+        return sciezka
 
     def dijkstra(self, startowy):
-        d = {w: float('inf') for w in self.wierzcholki}
-        d[startowy] = 0
+        d = {w: math.inf for w in self.wierzcholki}
         p = {w: None for w in self.wierzcholki}
-        QS = set()
+        d[startowy] = 0
+        Q = set(self.wierzcholki)
 
-        while QS != set(self.wierzcholki):
-            u = min((w for w in self.wierzcholki if w not in QS), key=lambda w: d[w])
-            QS.add(u)
-            for v, waga in self.krawedzie:
-                if v[0] == u and v[1] not in QS and d[u] + waga < d[v[1]]:
-                    d[v[1]] = d[u] + waga
-                    p[v[1]] = u
+        while Q:
+            u = min(Q, key=lambda x: d[x])
+            Q.remove(u)
+            for v, waga in self.graf[u].items():
+                alt = d[u] + waga
+                if alt < d[v]:
+                    d[v] = alt
+                    p[v] = u
 
         wyniki = {}
-        for w in self.wierzcholki:
-            sciezka = []
-            u = w
-            while u is not None:
-                sciezka.insert(0, u)
-                u = p[u]
-            wyniki[w] = (sciezka, d[w])
+        for wierzcholek in self.wierzcholki:
+            sciezka = self.znajdz_sciezke(p, wierzcholek)
+            koszt = d[wierzcholek]
+            wyniki[wierzcholek] = (sciezka, koszt)
+
         return wyniki
 
 if __name__ == "__main__":
     g_wierzcholki = ["s", "u", "x", "v", "y"]
-    g_krawedzie = [(("s","x"), 5), (("s","u"), 10), (("u","v"), 1), (("u","x"), 3), (("x","u"), 2), (("x","y"), 2), (("x","v"), 9), (("v","y"), 4), (("y","s"), 7), (("y","v"), 6)] 
+    g_krawedzie = [(("s", "x"), 5), (("s", "u"), 10), (("u", "v"), 1), (("u", "x"), 3), (("x", "u"), 2),
+                   (("x", "y"), 2), (("x", "v"), 9), (("v", "y"), 4), (("y", "s"), 7), (("y", "v"), 6)]
     g = Graf(g_wierzcholki, g_krawedzie)
-    wyniki_dijkstra = g.dijkstra("A")
+    wyniki_dijkstra = g.dijkstra("s")
     for wierzcholek, (sciezka, koszt) in wyniki_dijkstra.items():
         print(f"{wierzcholek}: {' '.join(sciezka)} koszt: {koszt}")
